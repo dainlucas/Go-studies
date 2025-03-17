a4d88d5f4157a53e696d289e557e95bb7a99908a
Lucas Ruan <lucasdainn901@gmail.com>
2025-03-16 22:41:00 -0300

    cria instância gin para rodar localmente

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+)
+
+//type Pizza struct {
+//ID int
+//nome string
+//preco float64
+//}
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	//criação de instância do gin
+	router := gin.Default()
+	router.GET("/pizzas", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"pizzas": "Frango com Catupiry",
+		})
+	})
+
+	router.Run() 
+
+	// declaração de variável nome da Pizzaria
+	//var nomePizzaria string = "Pizzaria do Go_rdo"
+
+	// declaração de variavel pizzas e seus objetos
+	//var pizzas = []Pizza{
+		//{ID: 1, nome: "Toscana", preco: 49.5},
+		//{ID: 2, nome: "Marguerita", preco: 79.5},
+		//{ID: 3, nome: "Atum com queijo", preco: 69.5},
+
+	//}
+
+	// impressão de variáveis
+	//fmt.Println(nomePizzaria)
+	//fmt.Println(pizzas)
 }
